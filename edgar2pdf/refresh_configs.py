009cfe108c2c4a7699b9c52631aec898bda3c0ed
kenta-sakai-0 <atneks@gmail.com>
2024-10-01 19:27:39 -0700

    Finished edgar2pdf
    Finished edgar2pdf scripts and restrucured folders

---
+++ edgar2pdf/refresh_configs.py
@@ -0,0 +1,16 @@
+watchlist = {
+    # "META": "0001326801",
+    # "AAPL": "0000320193",
+    # "NVDA": "0001045810",
+    # "ECDA": "0001922858"
+    }
+
+filingForm = [
+    "10-K", # Annual   
+    "10-Q", # Quarterly 
+    "8-K" # Press releases
+    ]
+
+# Inclusive dates
+filingDate_from = "2022-01-01"
+filingDate_to = "2023-01-01"
---
+++ edgar2pdf/test.py
@@ -0,0 +1,18 @@
+from check_edgar import check_for_new_filings
+# Testing for get_sec_filing.
+# download_sec_filing("meta", "0001193125-23-134410")
+# get_sec_filing("0001326801", "0001326801-24-000069")
+
+# get_sec_filing("nvda", "000104581024000124")
+
+# # Should throw an error
+# get_sec_filing("ada", "000095017023059393")
+    ### FIX LATER ###  
+
+# Test for poll_edgar   
+# check_for_new_filings("0001326801") #META
+# get_new_filing("0001922858")
+
+# get_filing("0001326801", "2024-01-01")
+
+# filing_exists("0001326801-24-lll")
